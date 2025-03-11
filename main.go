462aa75b22f0892223df8bf2f29afe8ac84f7e4e
Darius Rapalis <info@rdarius.lt>
2025-03-11 17:07:57 +0200

    Health Check

--- main.go
+++ main.go
@@ -4,10 +4,19 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir("./public")))
+	mux.HandleFunc("/healthz", readinessHandler)
+
+	fileServer := http.FileServer(http.Dir("./public"))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 
 	server := &http.Server{
 		Addr:    ":8080",
